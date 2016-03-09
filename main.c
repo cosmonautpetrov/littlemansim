a8cd7fc0466bb25b08e5d6d887d8cebf03c4ad91
Daniel Panasyuk <dpanasyu@ucsc.edu>
2016-03-08 19:03:45 -0800

    debug mode implemented

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ unsigned char (*op[OP_NUM])(struct lmc* src);
 int main(int argc, char* argv[]){
 	
 	//check for proper input
-	if(argc==1 || argc > 3){
+	if(argc==1 || argc > 4){
 		printf("%s: <filename> <memory length>\n",argv[0]);
 		return -1;
 	}
@@ -25,11 +25,16 @@ int main(int argc, char* argv[]){
 		return -1;
 	}
 
+	//check if debug flag set
+	if(argc > 3){
+			
+	}
+
 	//if everything checks out, create lmc and start simulating
 	struct lmc* sim = malloc(sizeof(struct lmc));
 	sim->dat = data;
 	sim->mem = malloc(sizeof(char)*mem_length);
-
+	sim->mem_size = mem_length;
 	//make table of function pointers to opcodes
 	op[0]=hlt;
 	op[1]=add;
@@ -44,6 +49,7 @@ int main(int argc, char* argv[]){
 	
 	//main loop, loop through loaded program until it ends
 	for(sim->ip=0;sim->dat[sim->ip]!='\0';){
+		print_debug(sim);
 		op[sim->dat[sim->ip]](sim);//call corresponding opcode
 	}
 
@@ -84,6 +90,19 @@ int cleanup(struct lmc* src){
 	free(src->mem);
 	free(src);
 	return 0;
+}
+
+int print_debug(struct lmc* src){
+	printf("---------------------------\n");
+	printf("ip: %i reg: %i mem_size: %i inst: %i\n",src->ip,src->reg,src->mem_size,src->dat[src->ip]);
+	int i;
+	for(i=0;i<src->mem_size;i++){
+		if(i%10==0)
+			printf("\n%i: ",i);
+		printf("%i ",src->mem[i]);
+	}
+	printf("\n---------------------------\n");
+	return 0;	
 }
 
 unsigned char hlt(struct lmc* src){
